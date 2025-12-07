0ad93133718f68e48c41a5cfcd18f3df52a379c3
James Sizeland <jamessizeland@gmail.com>
2025-12-07 16:23:34 +0000

    add more general parsing traits

--- aoc/src/helpers/parsing.rs
+++ aoc/src/helpers/parsing.rs
@@ -1,3 +1,5 @@
+use anyhow::Result;
+use colored::*;
 use regex::Regex;
 use std::sync::OnceLock;
 
@@ -22,14 +24,51 @@ pub fn extract_u64s(input: &str) -> Vec<u64> {
         .collect()
 }
 
-/// A trait to allow parsing directly on strings
-/// Usage: "10, -5".nums()
 pub trait ParseOps {
+    /// A trait to allow parsing directly on strings
+    /// Usage: "10, -5".nums() -> vec![10,-5]
     fn nums(&self) -> Vec<isize>;
+
+    /// Split into individual characters
+    /// Usage: "12345".digits() -> vec![1,2,3,4,5]
+    fn digits(&self) -> Vec<u32>;
+
+    /// Paint the specified indexes RED.
+    fn colour_by_index(&self, indexes: &[usize]) -> String;
 }
 
 impl ParseOps for str {
     fn nums(&self) -> Vec<isize> {
         extract_numbers(self)
+    }
+
+    fn digits(&self) -> Vec<u32> {
+        self.chars().map(|c| c.to_digit(10).unwrap()).collect()
+    }
+
+    fn colour_by_index(&self, indexes: &[usize]) -> String {
+        let mut output = String::new();
+
+        for (idx, ch) in self.chars().enumerate() {
+            let s = ch.to_string();
+            if indexes.contains(&idx) {
+                output.push_str(&format!("{}", s.red()));
+            } else {
+                output.push_str(&format!("{}", s.green()));
+            }
+        }
+
+        output
+    }
+}
+
+pub trait CombineOps {
+    /// Combine two numbers into a single number
+    fn combine(&self) -> Result<usize>;
+}
+
+impl CombineOps for &(u32, u32) {
+    fn combine(&self) -> Result<usize> {
+        Ok(format!("{}{}", self.0, self.1).parse()?)
     }
 }
