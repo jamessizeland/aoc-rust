5f5cf821ea318112e727dcb1555bbe5ced2b7fc1
James Sizeland <jamessizeland@gmail.com>
2025-12-07 15:10:09 +0000

    complete day 3 part a

--- puzzles/src/bin/2025_day03.rs
+++ puzzles/src/bin/2025_day03.rs
@@ -1,32 +1,48 @@
 use aoc::prelude::*;
+
+fn colour_indexes(input: &str, indexes: &[usize]) {
+    let chars = input.chars().collect_vec();
+    for (idx, ch) in chars.iter().enumerate() {
+        let s = ch.to_string();
+        if indexes.contains(&idx) {
+            print!("{}", s.red());
+        } else {
+            print!("{}", s.green());
+        }
+    }
+}
+fn combine_nums(nums: &(u32, u32)) -> Result<usize> {
+    Ok(format!("{}{}", nums.0, nums.1).parse()?)
+}
+
 #[allow(unused)]
 fn part_a(input: &Input) -> Result<usize> {
     let mut output = 0;
     let lines = input.lines().collect_vec();
     for line in lines {
         let digits = line.chars().map(|c| c.to_digit(10).unwrap()).collect_vec();
-        let mut largest = (0, 0);
+        let mut largest_joltage = 0;
+        let mut refs = (0, 0);
         'outer: for pos in 0..(digits.len() - 1) {
             let first = digits[pos];
-            if first < largest.0 {
-                continue;
-            }
-            largest = (first, 0); // reset second value
-            // println!("first -> {first}");
+            let mut largest = (first, 0); // reset second value
+            let mut new_refs = (pos, 0);
             for pos in (pos + 1)..digits.len() {
                 let second = digits[pos];
-                // println!("second -> {second}");
                 if second > largest.1 {
                     largest.1 = second;
-                    if first == 9 && second == 9 {
-                        break 'outer; // found max possible Joltage
-                    }
+                    new_refs.1 = pos;
                 }
             }
+            let new_joltage = combine_nums(&largest)?;
+            if new_joltage >= largest_joltage {
+                largest_joltage = new_joltage;
+                refs = new_refs;
+            }
         }
-        let joltage: usize = format!("{}{}", largest.0, largest.1).parse()?;
-        println!("{line} -> {joltage}");
-        output += joltage;
+        colour_indexes(line, &[refs.0, refs.1]);
+        println!(" -> {largest_joltage}");
+        output += largest_joltage;
     }
 
     Ok(output)
@@ -59,9 +75,9 @@ mod test {
     }
     #[test]
     fn test_part_a2() {
-        let input = Input::from_literal("9798");
+        let input = Input::from_literal("8586");
         let result = part_a(&input).unwrap();
-        assert_eq!(result, 99);
+        assert_eq!(result, 88);
     }
 
     #[test]
